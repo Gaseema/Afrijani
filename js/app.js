5abfefeab4bf8b72bf5d8d637ce96b781ec82391
Gaseema Ndung'u <gaseema.n@gmail.com>
2016-05-27 10:36:32 +0300

    added about, whatwedo pages

--- js/app.js
+++ js/app.js
@@ -12,17 +12,3 @@ $(function () {
     });
 
 });
-
-
-
-//serchbar
-$(document).ready(function () {
-    $(".search-wrapper input").mouseenter(function () {
-        $(".search-wrapper button").css("background-color", "#4aba10");
-
-
-    });
-    $(".search-wrapper input").mouseout(function () {
-        $(".search-wrapper button").css("background-color", "");
-    });
-});
